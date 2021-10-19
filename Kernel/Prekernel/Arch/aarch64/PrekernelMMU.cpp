cdcf1c05b49a66bad0d1d795fa889b40852d4ff4
James Mintram <me@jamesrm.com>
2021-10-19 19:02:52 +0100

    Kernel: Refactor prekernel MMU to use a bump allocator

--- Kernel/Prekernel/Arch/aarch64/PrekernelMMU.cpp
+++ Kernel/Prekernel/Arch/aarch64/PrekernelMMU.cpp
@@ -37,6 +37,7 @@ constexpr u32 PAGE_TABLE_SIZE = 0x1000;
 // https://developer.arm.com/documentation/101811/0101/Controlling-address-translation
 constexpr u32 PAGE_DESCRIPTOR = 0b11;
 constexpr u32 TABLE_DESCRIPTOR = 0b11;
+constexpr u32 DESCRIPTOR_MASK = ~0b11;
 
 constexpr u32 ACCESS_FLAG = 1 << 10;
 
@@ -48,44 +49,85 @@ constexpr u32 INNER_SHAREABLE = (3 << 8);
 constexpr u32 NORMAL_MEMORY = (0 << 2);
 constexpr u32 DEVICE_MEMORY = (1 << 2);
 
+constexpr uint64_t* descriptor_to_pointer(uint64_t descriptor)
+{
+    return (uint64_t*)(descriptor & DESCRIPTOR_MASK);
+}
+
 typedef unsigned char* page_table_t;
 
-static void zero_identity_map(page_table_t page_table_start, page_table_t page_table_end)
+static void zero_pages(uint64_t* start, uint64_t* end)
 {
     // Memset all page table memory to zero
-    for (uint64_t* p = (uint64_t*)page_table_start;
-         p < (uint64_t*)page_table_end;
+    for (uint64_t* p = (uint64_t*)start;
+         p < (uint64_t*)end;
          p++) {
 
         *p = 0;
     }
 }
 
-static void build_identity_map(page_table_t page_table)
+namespace {
+class PageBumpAllocator {
+public:
+    PageBumpAllocator(uint64_t* start, uint64_t* end)
+        : m_start(start)
+        , m_end(end)
+        , m_current(start)
+    {
+        if (m_start >= m_end) {
+            Prekernel::panic("Invalid memory range passed to PageBumpAllocator");
+        }
+        if ((uint64_t)m_start % PAGE_TABLE_SIZE != 0 || (uint64_t)m_end % PAGE_TABLE_SIZE != 0) {
+            Prekernel::panic("Memory range passed into PageBumpAllocator not aligned to PAGE_TABLE_SIZE");
+        }
+    }
+
+    uint64_t* TakePage()
+    {
+        if (m_current == m_end) {
+            Prekernel::panic("Prekernel pagetable memory exhausted");
+        }
+
+        uint64_t* page = m_current;
+        m_current += (PAGE_TABLE_SIZE / sizeof(uint64_t));
+
+        zero_pages(page, page + (PAGE_TABLE_SIZE / sizeof(uint64_t)));
+        return page;
+    }
+
+private:
+    const uint64_t* m_start;
+    const uint64_t* m_end;
+    uint64_t* m_current;
+};
+}
+
+static void build_identity_map(PageBumpAllocator& allocator)
 {
-    // Set up first entry of level 1
-    uint64_t* level1_entry = (uint64_t*)page_table;
-    *level1_entry = (uint64_t)&page_table[PAGE_TABLE_SIZE];
-    *level1_entry |= TABLE_DESCRIPTOR;
+    uint64_t* level1_table = allocator.TakePage();
 
-    // Set up first entry of level 2
-    uint64_t* level2_entry = (uint64_t*)&page_table[PAGE_TABLE_SIZE];
-    *level2_entry = (uint64_t)&page_table[PAGE_TABLE_SIZE * 2];
-    *level2_entry |= TABLE_DESCRIPTOR;
+    level1_table[0] = (uint64_t)allocator.TakePage();
+    level1_table[0] |= TABLE_DESCRIPTOR;
 
-    // Set up L3 entries
+    uint64_t* level2_table = descriptor_to_pointer(level1_table[0]);
+    level2_table[0] = (uint64_t)allocator.TakePage();
+    level2_table[0] |= TABLE_DESCRIPTOR;
+
+    uint64_t* level3_table = descriptor_to_pointer(level2_table[0]);
+
+    // // Set up L3 entries
     for (uint32_t l3_idx = 0; l3_idx < 512; l3_idx++) {
-        uint64_t* l3_entry = (uint64_t*)&page_table[PAGE_TABLE_SIZE * 2 + (l3_idx * sizeof(uint64_t))];
-
-        *l3_entry = (uint64_t)(page_table + (PAGE_TABLE_SIZE * 3) + (l3_idx * PAGE_TABLE_SIZE));
-        *l3_entry |= TABLE_DESCRIPTOR;
+        level3_table[l3_idx] = (uint64_t)allocator.TakePage();
+        level3_table[l3_idx] |= TABLE_DESCRIPTOR;
     }
 
     // Set up L4 entries
     size_t page_index = 0;
 
     for (size_t addr = START_OF_NORMAL_MEMORY; addr < END_OF_NORMAL_MEMORY; addr += GRANULE_SIZE, page_index++) {
-        uint64_t* l4_entry = (uint64_t*)&page_table[PAGE_TABLE_SIZE * 3 + (page_index * sizeof(uint64_t))];
+        uint64_t* level4_table = descriptor_to_pointer(level3_table[page_index / 512]);
+        uint64_t* l4_entry = &level4_table[page_index % 512];
 
         *l4_entry = addr;
         *l4_entry |= ACCESS_FLAG;
@@ -96,7 +138,8 @@ static void build_identity_map(page_table_t page_table)
 
     // Set up entries for last 16MB of memory (MMIO)
     for (size_t addr = START_OF_DEVICE_MEMORY; addr < END_OF_DEVICE_MEMORY; addr += GRANULE_SIZE, page_index++) {
-        uint64_t* l4_entry = (uint64_t*)&page_table[PAGE_TABLE_SIZE * 3 + (page_index * sizeof(uint64_t))];
+        uint64_t* level4_table = descriptor_to_pointer(level3_table[page_index / 512]);
+        uint64_t* l4_entry = &level4_table[page_index % 512];
 
         *l4_entry = addr;
         *l4_entry |= ACCESS_FLAG;
@@ -149,8 +192,8 @@ static void activate_mmu()
 
 void init_prekernel_page_tables()
 {
-    zero_identity_map(page_tables_phys_start, page_tables_phys_end);
-    build_identity_map(page_tables_phys_start);
+    PageBumpAllocator allocator((uint64_t*)page_tables_phys_start, (uint64_t*)page_tables_phys_end);
+    build_identity_map(allocator);
     switch_to_page_table(page_tables_phys_start);
     activate_mmu();
 }
