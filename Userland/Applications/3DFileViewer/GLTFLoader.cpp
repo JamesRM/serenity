ff7bb920444055852aceeefbe3a5aa6969f594a5
James Mintram <me@jamesrm.com>
2022-04-02 00:01:24 +0100

    3DViewer: Parse the GLTF file into a JSON value

--- Userland/Applications/3DFileViewer/GLTFLoader.cpp
+++ Userland/Applications/3DFileViewer/GLTFLoader.cpp
@@ -6,7 +6,22 @@
 
 #include "GLTFLoader.h"
 
-RefPtr<Mesh> GLTFLoader::load(Core::File&)
+#include <LibCore/File.h>
+
+#include <AK/JsonParser.h>
+
+RefPtr<Mesh> GLTFLoader::load(Core::File& file)
 {
+    dbgln("GLTF: Try to load as GLTF");
+
+    auto parsed_result = JsonParser(file.read_all()).parse();
+
+    if (parsed_result.is_error()) {
+        dbgln("GLTF: Error parsing JSON {}", parsed_result.error());
+        return nullptr;
+    }
+
+    dbgln("GLTF: JSON loaded successfuly");
+
     return nullptr;
 }
